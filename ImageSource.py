8b93b87487a92ffc1a6326c4a6ae64e706b06343
Joey Slater <joeyslater@gatech.edu>
2015-11-20 00:03:51 -0600

    - inputs the images and returns False if no Primary and Secondary

--- ImageSource.py
+++ ImageSource.py
@@ -1,5 +1,9 @@
-class ImageSource:
+import cv2
+import os
+import re
 
+
+class ImageSource:
     def __init__(self):
         self.primary_image = None
         self.secondary_images = []
@@ -12,7 +16,35 @@ class ImageSource:
             - at least 1 secondary image was found
 
         Naming convention:
-            Each folder will have one "Primary.jpg" image and at least one "Secondary-x.jpg" image,
+            Each folder will have one "Primary.ext" image and at least one "Secondary-x.ext" image,
             where x corresponds to the image number.
+            :param folder_path: String
         """
-        pass
+
+        # Most image load comes from Assignment 8 test script
+        # Extensions recognized by OpenCV
+        extensions = ['.bmp', '.pbm', '.pgm', '.ppm', '.sr', '.ras', '.jpeg',
+                      '.jpg', '.jpe', '.jp2', '.tiff', '.tif', '.png']
+
+        secondary_names_to_sort = []
+        for dir_name, dir_names, file_names in os.walk(folder_path):
+            for filename in file_names:
+                name, ext = os.path.splitext(filename)
+                if ext.lower() in extensions:
+                    if name.lower() == 'primary':
+                        self.primary_image = cv2.imread(os.path.join(dir_name, filename))
+                    elif 'secondary-' in name.lower():
+                        secondary_names_to_sort.append(filename)
+            secondary_names_to_sort = self.__natural_sort(secondary_names_to_sort)
+            for secondary in secondary_names_to_sort:
+                self.secondary_images.append(cv2.imread(os.path.join(dir_name, secondary)))
+
+        # is not [] is only necessary if you want the boolean and not the falsy/truthy value
+        return self.primary_image is not None and self.secondary_images is not []
+
+    # Added to sort the files for 1 vs 01 vs 10, etc
+    # Taken from https://stackoverflow.com/qsuestions/11150239/python-natural-sorting
+    def __natural_sort(self, l):
+        convert = lambda text: int(text) if text.isdigit() else text.lower()
+        alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
+        return sorted(l, key = alphanum_key)
